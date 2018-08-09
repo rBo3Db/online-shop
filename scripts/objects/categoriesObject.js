028a93bd4ad070693c16a8dc282273550790759b
Aleksei Chernitsyn <Aleksei_Chernistyn@epam.com>
2018-08-09 18:52:41 +0400

    started to do templates

---
+++ scripts/objects/categoriesObject.js
@@ -0,0 +1,18 @@
+var products = [
+    {  
+        name:'cut-off wheels',
+        photo: './pics/categories-grid/big_91a0aec1f7e49f08f81c7c25bcce9fd1_01.jpg',
+    },
+    {
+        name:'grinding wheels',
+        photo: './pics/categories-grid/big_91a0aec1f7e49f08f81c7c25bcce9fd1_1.jpg',
+    },
+    {
+        name:'chisels',
+        photo: './pics/categories-grid/big_91a0aec1f7e49f08f81c7c25bcce9fd1_12.jpg',
+    },
+    {
+        name:'speakers',
+        photo: './pics/categories-grid/big_8018582e5b743eec172a260d99602c0f_0.jpg',
+    }     
+];
