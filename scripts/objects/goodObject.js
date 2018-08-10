623b08a11667a8a8dd6d0e2cf4b2d084387bdafb
Aleksei Chernitsyn <Aleksei_Chernistyn@epam.com>
2018-08-10 14:32:05 +0400

    1

--- scripts/objects/goodObject.js
+++ scripts/objects/goodObject.js
@@ -1,18 +1,34 @@
 var goods = [
     {  
-        name:'cut-off wheels',
-        photo: './pics/categories-grid/big_91a0aec1f7e49f08f81c7c25bcce9fd1_01.jpg',
+        name:'cut-off wheel',
+        photo: './pics/cart/googlenexus6p-1024x768.jpg',
+        price: 555,
+        description: 'Good for cut',
+        quantity: 100,
+        category: 'cut-off wheels',
     },
     {
-        name:'grinding wheels',
+        name:'grinding wheel',
         photo: './pics/categories-grid/big_91a0aec1f7e49f08f81c7c25bcce9fd1_1.jpg',
+        price: 555,
+        description: 'Good for grind',
+        quantity: 100,
+        category: 'grinding wheel',
     },
     {
-        name:'chisels',
+        name:'chisel',
         photo: './pics/categories-grid/big_91a0aec1f7e49f08f81c7c25bcce9fd1_12.jpg',
+        price: 555,
+        description: 'Good for chisels',
+        quantity: 100,
+        category: 'chisels',
     },
     {
-        name:'speakers',
+        name:'speaker',
         photo: './pics/categories-grid/big_8018582e5b743eec172a260d99602c0f_0.jpg',
-    }     
+        price: 555,
+        description: 'Good for speakering',
+        quantity: 100,
+        category: 'speakers',
+    },
 ];
