c0234e4bc822d5102093b95cdac4d697fb15861f
Aleksei Chernitsyn <Aleksei_Chernistyn@epam.com>
2018-08-15 15:25:38 +0400

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,63 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var browserify = require('browserify');
+var browserSync = require('browser-sync');
+source = require('vinyl-source-stream');
+const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify');
 // var source = require('gulp-bundle');
+gulp.task('html', function() {
+    gulp.src('/*.html')
+        .pipe(gulp.dest('./app/html'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+gulp.task('img', function() {
+    gulp.src('.img/**/**/*.html')
+        .pipe(gulp.dest('./app/img'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
 gulp.task('sass', function() {
     return gulp.src('./styles/main.scss') 
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['> 0.1%'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./app/artifacts'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 })
 
 gulp.task('js', function() {
     gulp.src('./scripts/**/*.js')
     .pipe(concat('min.js'))
+    .pipe(minify())
     .pipe(gulp.dest('./app/js/'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 gulp.task('browserify', function() { 
 return browserify('./src/javascript/app.js') 
     .bundle() 
     .pipe(source('bundle.js')) 
-    .pipe(gulp.dest('.app/build/')); 
+    .pipe(gulp.dest('.app/build/'));
+    
 });
 
 gulp.task('watch', function(){
     gulp.watch('./scss/**/**/*.scss',['sass']);
+    gulp.watch('./*.html',['html']);
+    
     // another files to  watch
 })
-var browserSync = require('browser-sync');
+
 gulp.task('browserSync', function() { 
 	browserSync({ 
 		server: { 	
@@ -48,4 +81,4 @@ gulp.task('default', function() {
 
 // require('handlebars');
 
-gulp.task('default', ['sass','js','watch', 'browserSync']);
+gulp.task('default', ['img','html','sass','js','watch', 'browserSync']);
